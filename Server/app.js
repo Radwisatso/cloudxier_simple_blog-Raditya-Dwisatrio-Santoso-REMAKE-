a8a7153ce367431c2140508da2aa0dbebbc01dd5
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2022-01-02 09:45:08 +0700

    Fixing Port

--- Server/app.js
+++ Server/app.js
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Listening on port ${port}`)
 })
