8132689818daf53bb6af5240eadcf95233765085
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2022-01-02 09:48:25 +0700

    FIxing App 2

--- Server/app.js
+++ Server/app.js
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Listening on port ${port}`)
 })
