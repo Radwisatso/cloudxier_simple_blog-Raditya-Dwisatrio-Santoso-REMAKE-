f21afdb01d0b294c62704500610eaa05f18d5e02
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2022-01-02 21:47:09 +0700

    Fetch data at Home Page

--- Client/cloudxier-fish-blog/src/pages/Home.js
+++ Client/cloudxier-fish-blog/src/pages/Home.js
@@ -1,25 +1,50 @@
-import React, {useState} from "react"
-import { Button } from 'antd'
+import React, {useEffect, useState} from "react"
+import { Button, Card } from 'antd'
 import { Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
 
 const Home = () => {
+    const { Meta } = Card
     const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://cloudxier-blog.herokuapp.com/fishes`, data)
+        .then((res => {
+            console.log("response", res.data)
+            setData(res.data)
+        }))
+        .catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
     return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            alignContent: "center"
-            }}>
-            <h2>
+        <div>
+            <h2 style={{fontSize: "150%"}}>
                 Welcome to the Fish Blog
             </h2>
-            <Button block style={{margin:"20px", width: "50%", fontSize: "13px"}}>
+            <Button 
+            block 
+            style={{margin:"20px", width: "50%", fontSize: "13px"}}
+            >
                 <Link to={"/add"}>Click here to add more fish!</Link>
             </Button>
-            <p>
-                {JSON.stringify(data)}
-            </p>
+            <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+                {
+                    data.map((fish) => (
+                        <Card
+                        key={fish.id}
+                        style={{display: "flex", flexWrap: "wrap"}}
+                        hoverable
+                        style={{ width: 240, margin: 20 }}
+                        cover={<img alt="example" src={fish.image} />}
+                        >
+                            <Meta title={fish.name} description={fish.description} />
+                            
+                        </Card>
+                    ))
+                }
+            </div>
         </div>
     )
 }
