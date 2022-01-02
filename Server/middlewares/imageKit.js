18a714a00a5f308723e00bd2ddb0d464ffb2a4a3
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2022-01-02 15:35:09 +0700

    Enabling ImageKit without buffer

--- Server/middlewares/imageKit.js
+++ Server/middlewares/imageKit.js
@@ -7,26 +7,30 @@ const FormData = require("form-data")
 
 imageKit = async (req, res, next) => {
     try {
-        const newForm = new FormData()
-        const encodedFile = req.file.buffer.toString("base64")
-        newForm.append("file", encodedFile)
-        newForm.append("fileName", req.file.originalname)
-        const encodedKey = Buffer.from(process.env.imageKitPrivate+":").toString("base64")
-        const result = await axios({
-            method: "POST",
-            url: "https://upload.imagekit.io/api/v1/files/upload",
-            data: newForm,
-            headers: {
-                ...newForm.getHeaders(),
-                Authorization: `Basic ${encodedKey}`
-            }
-        })
-        console.log(result)
-        if (!result) {
-            throw ({name: "Bad Request"})
-        } else {
-            req.body.fishImage = result.data.url
+        if (req.params.id && !req.file) {
             next()
+        } else {
+            const newForm = new FormData()
+            const encodedFile = req.file.buffer.toString("base64")
+            newForm.append("file", encodedFile)
+            newForm.append("fileName", req.file.originalname)
+            const encodedKey = Buffer.from(process.env.imageKitPrivate+":").toString("base64")
+            const result = await axios({
+                method: "POST",
+                url: "https://upload.imagekit.io/api/v1/files/upload",
+                data: newForm,
+                headers: {
+                    ...newForm.getHeaders(),
+                    Authorization: `Basic ${encodedKey}`
+                }
+            })
+            console.log(result)
+            if (!result) {
+                throw ({name: "Bad Request"})
+            } else {
+                req.body.fishImage = result.data.url
+                next()
+            }
         }
     } catch (err) {
         next(err)
