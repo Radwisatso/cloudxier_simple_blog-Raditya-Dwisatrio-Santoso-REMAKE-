750976da6c8be1bce41cc2a518f5990b66aca5c4
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2022-01-02 09:50:28 +0700

    Still fixing App

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config()
-}
+// if (process.env.NODE_ENV !== "production") {
+//     require("dotenv").config()
+// }
 
 const express = require("express")
 const app = express()
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
